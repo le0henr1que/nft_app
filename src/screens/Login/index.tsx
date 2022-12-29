b3db51cf278c8c2eb18f49cd27d8f7506547797e
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-12-28 21:17:04 -0300

    feat: style login page

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,19 +1,41 @@
-import { Image } from 'react-native';
+import React from 'react';
+import { Image, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 // import { Background } from "../../Components/Background"
 import ImageIcon from "../../assets/Frame39.png";
 import { Button } from "../../Components/Button"
 import { styles } from "./styles"
 export function Login(){
+    const [text, onChangeText] = React.useState();
     return (
         <SafeAreaView style={styles.container}>
             <Image
                 // style={styles.tinyLogo}
                 source={ImageIcon}
             />
-                <Button background={true} text="Entrar"/>
-                
-                <Button background={false} text="Entrar com a Metamask"/>
+
+            <Text style={styles.textBold}>Crypto</Text>
+            <Text style={styles.textRegular}>O maior marketplace de NFTs do Brasil</Text>
+
+            <TextInput
+                style={styles.emailInput}
+                onChangeText={onChangeText}
+                value={text}
+            />
+            <TextInput
+                 style={styles.passInput}
+                 onChangeText={onChangeText}
+                 value={text}
+                 secureTextEntry={true}
+            />
+            <Text style={styles.textRegularPurple}>recuperar a senha?</Text>
+
+
+            <Button background={true} text="Entrar"/>
+
+                <View style={styles.horizontalLine}></View>
+
+            <Button background={false} text="Entrar com a Metamask"/>
         </SafeAreaView>
     );
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -4,5 +4,64 @@ export const styles = StyleSheet.create({
     container: {
       flex:1, 
       alignItems:'center'
+    }, 
+    textBold:{
+      color:"#FFF", 
+      fontSize:32,
+      // fontWeight:700
+      lineHeight:40
+    }, 
+    textRegular:{
+      color:"#FFFFFF", 
+      opacity: 0.5, 
+      fontSize: 14, 
+      marginTop: 4, 
+      lineHeight:20, 
+    }, 
+
+    horizontalLine:{
+        width:171, 
+        height:1, 
+        backgroundColor:"#FFF", 
+        marginBottom:48, 
+        marginTop:48, 
+        opacity: 0.2
+    }, 
+    emailInput:{
+      width:342, 
+      height:48, 
+      borderColor:"#474053", 
+      borderWidth:2, 
+      backgroundColor:"#211D28", 
+      marginTop:32, 
+      borderTopLeftRadius:12,
+      borderTopRightRadius:12, 
+      padding:12,
+      fontSize:14, 
+      color:"#FFF", 
+      opacity: 0.5
+    }, 
+    passInput:{
+      width:342, 
+      height:48, 
+      borderColor:"#474053", 
+      borderWidth:2, 
+      backgroundColor:"#211D28", 
+      borderBottomLeftRadius:12, 
+      borderBottomRightRadius:12, 
+      marginTop:-2, 
+      padding:12, 
+      fontSize:14, 
+      color:"#FFF", 
+      opacity: 0.5
+
+    }, 
+    textRegularPurple:{
+      color:"#9A4DFF", 
+      opacity: 0.5, 
+      fontSize: 14, 
+      marginTop: 4, 
+      left:112, 
+      marginBottom:32
     }
 });
